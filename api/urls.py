c5d07a772077d60d97a0867503011a8c8f8e5a4c
NikitaZ <eretomaniac@gmail.com>
2023-03-31 13:52:44 +0300

    restapi

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
-#from api.views import CheckboxViewSet
+from api.views import CheckboxViewSet
 from api import views
 
-#router = routers.DefaultRouter()
-#router.register('checkbox', CheckboxViewSet)
+router = routers.DefaultRouter()
+router.register('checkbox', CheckboxViewSet)
 
 urlpatterns = [
-    #path('', include(router.urls)),
+    path('', include(router.urls)),
     #path('checkbox_list', views.checkbox_list, name='checkbox_list'),
     #path('checkbox_list/<int:pk>', views.checkbox_detail, name='checkbox_list'),
     #path('checkbox_created', views.checkbox_created, name='checkbox_created'),
     #path('checkbox_update/<int:pk>', views.checkbox_update, name='checkbox_update'),
     #path('checkbox_delete/<int:pk>', views.checkbox_delete, name='checkbox_delete'),
-    path('checkbox_list', views.CheckboxList.as_view(), name='user_list'),
+    #path('checkbox_list', views.CheckboxViewSet.as_view(), name='user_list'),
+    path('data', views.DataView.as_view()),
 ]
